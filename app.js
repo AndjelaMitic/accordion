6d5e748efe7fa5447fe726cddd5e6ec071be0252
AndjelaMitic <missandjelaa@gmail.com>
2021-07-06 13:33:54 +0200

    + funcionality 1.1

--- app.js
+++ app.js
@@ -11,31 +11,51 @@ accordion.classes = {
 
 accordion.states = {
   itemSelected: 'accordion__item--selected',
-  contentOpen: 'accordion__content--open'
+  contentOpen: 'accordion__content--open',
+  arrowUp: 'fa-chevron-up',
+  arrowDown: 'fa-chevron-down'
 
 },
 
 accordion.selectors = {
-  ctx : document.getElementsByClassName(accordion.classes.context),
-  item: document.getElementsByClassName(accordion.classes.item),
-  arrow : document.getElementsByClassName(accordion.classes.arrow),
-  content : document.getElementsByClassName(accordion.classes.content)
+  ctx : document.querySelector(`.${accordion.classes.context}`),
+  item: document.querySelectorAll(`.${accordion.classes.item}`),
+  arrow : document.querySelectorAll(`.${accordion.classes.arrow}`),
+  content : document.querySelectorAll(`.${accordion.classes.content}`)
 };
 
 // DOM selectors
-const items = Array.from(accordion.selectors.item);
+const items = accordion.selectors.item;
 
-accordion.hideAll = () => {
-  items.forEach((item, i) => {
-    if (item.classList.contains(accordion.states.itemSelected)) {
-        item.classList.remove(accordion.states.itemSelected);
+accordion.deselecting = (item) => {
+  item.classList.remove(accordion.states.itemSelected);
+  item.querySelector(`.${accordion.classes.content}`).classList.remove(accordion.states.contentOpen);
+  item.querySelector(`.${accordion.classes.arrow}`).classList.remove(accordion.states.arrowUp);
+  item.querySelector(`.${accordion.classes.arrow}`).classList.add(accordion.states.arrowDown);
+},
+
+accordion.selecting = (item) => {
+  accordion.closeAll();
+  item.classList.add(accordion.states.itemSelected);
+  item.querySelector(`.${accordion.classes.content}`).classList.add(accordion.states.contentOpen);
+  item.querySelector(`.${accordion.classes.arrow}`).classList.remove(accordion.states.arrowDown);
+  item.querySelector(`.${accordion.classes.arrow}`).classList.add(accordion.states.arrowUp);
+},
+
+accordion.closeAll = () => {
+    items.forEach((item, i) => {
+    if (item.classList.contains(accordion.states.itemSelected) || item.querySelector(`.${accordion.classes.content}`).classList.contains(accordion.states.contentOpen)) {
+      accordion.deselecting(item);
     }
   });
 },
 
 accordion.showFirst = () => {
-  accordion.hideAll();
-  items[0].classList.add(accordion.states.itemSelected);
+  accordion.selecting(items[0]);
+},
+
+accordion.showSelected = (selected) => {
+  accordion.selecting(selected);
 },
 
 accordion.setEvent = () => {
@@ -44,17 +64,11 @@ accordion.setEvent = () => {
       accordion.showSelected(this);
     });
   });
-}
-
-accordion.showSelected = (selected) => {
-  accordion.hideAll();
-  selected.classList.add(accordion.states.itemSelected);
-  console.log(selected);
 },
 
 accordion.init = () => {
   accordion.showFirst();
   accordion.setEvent();
-},
+};
 
 accordion.init();
